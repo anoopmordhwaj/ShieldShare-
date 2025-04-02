90c27dfef7e011859aee375a58bced21e9f2ed4b
Anoop Mordhwaj <anoopmordhwaj5750@gmail.com>
2025-04-03 02:09:52 +0530

    Updated ALLOWED_HOSTS

--- file_sharing/settings.py
+++ file_sharing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l4e&+#qbb579ve-!z3@96d6==xsy$+x7=&8on0t=abis*@f$dg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shieldshare-whho.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
